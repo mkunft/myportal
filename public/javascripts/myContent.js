4afb5890948be37b4e9d02e40e35abedf076ac20
Martin Kunft <martin.kunft@ims-evolve.com>
2014-11-25 16:06:18 +0000

    Fixed working demo

---
+++ public/javascripts/myContent.js
@@ -0,0 +1,39 @@
+var myContent = {
+
+    Home: {
+        Content: "<h4>Getting Yesterday ready for Tomorrow</h4><p>Thank you for visiting the Kunft Protal. This site is primarily intended to serve as an active resume to showcase our abilities in Web Application and Software Development. With over 15 years of experience, the goal is to demonstrate not only design and usability aspects but also effective logic, solution, maintainability and deployment concepts.</p><p>With technology always moving forward, having an active representation of implementing these technologies is invaluable, giving any perspective employers an insight into the quality of our work.</p><p>Most of the work we have done is the Intellectual Property of our current employer, so we cannot show real life examples of this, but we have created demos that will give a conceptual idea without compromising our employer or infringing on their copyright.</p><p><i>Martin and Jana Kunft</i></p>",
+        Images: [
+            '/images/MKMap.png',
+            '/images/MKCity.jpg',
+            '/images/MKSat.png'
+        ]
+
+    },
+    Skills: {
+        Content: "<h4>Where there's Will there's a Way</h4><p>Javascript Perl Database bla bla bla</p>",
+        Images: [
+            '/images/MKMap.png',
+            '/images/MKCity.jpg',
+            '/images/MKSat.png'
+        ]
+
+    },
+    Work: {
+        Content: "<h4>Bread and Butter</h4><p>So IMS is a company yadda yadda yadda</p>",
+        Images: [
+            '/images/MKMap.png',
+            '/images/MKCity.jpg',
+            '/images/MKSat.png'
+        ]
+
+    },
+    Demos: {
+        Content: "<h4>Show more Tell less</h4><p>Try this .... No this .... But this .... what about this</p>",
+        Images: [
+            '/images/MKMap.png',
+            '/images/MKCity.jpg',
+            '/images/MKSat.png'
+        ]
+
+    }
+};
